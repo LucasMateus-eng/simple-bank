b193ff4d614e99984aacbc16bd080e409938fbb2
Lucas Neris <lucas.lima.neris@gmail.com>
2023-02-14 14:01:20 -0300

    refactor(simple-bank): ajustes no dto primário de Wallet

--- dto/primary/aggregate/wallet_dto.go
+++ dto/primary/aggregate/wallet_dto.go
@@ -4,8 +4,6 @@ import (
 	"errors"
 
 	"github.com/LucasMateus-eng/simple-bank/application/aggregate"
-	"github.com/LucasMateus-eng/simple-bank/application/entity"
-	valueobject "github.com/LucasMateus-eng/simple-bank/application/value_object"
 	apientity "github.com/LucasMateus-eng/simple-bank/dto/primary/entity"
 	apivalueobject "github.com/LucasMateus-eng/simple-bank/dto/primary/value_object"
 )
@@ -78,57 +76,4 @@ func (wa *WalletAPI) FromAggregate(wallet aggregate.Wallet) {
 	wa.Account = account
 	wa.Entries = entries
 	wa.Transfers = transfers
-}
-
-type WalletForUpdateAPI struct {
-	Person    apientity.PersonAPI          `json:"person"`
-	Account   apientity.AccountAPI         `json:"account"`
-	Entries   []apientity.EntryAPI         `json:"entries"`
-	Transfers []apivalueobject.TransferAPI `json:"transfers"`
-}
-
-func (wua *WalletForUpdateAPI) ToAggregate() (*aggregate.Wallet, error) {
-	if wua == nil {
-		return nil, errors.New("o dto do agregado Wallet não pode ser vazio")
-	}
-
-	person, err := wua.Person.ToEntity()
-	if err != nil {
-		return nil, err
-	}
-
-	account, err := wua.Account.ToEntity()
-	if err != nil {
-		return nil, err
-	}
-
-	wallet, err := aggregate.NewWallet(person.Name, person.PersonalIdentification, person.Email, person.Password, person.IsAShopkeeper)
-	if err != nil {
-		return nil, err
-	}
-
-	entries := make([]*entity.Entry, len(wua.Entries))
-	for _, eta := range wua.Entries {
-		et, err := eta.ToEntity()
-		if err != nil {
-			return nil, err
-		}
-		entries = append(entries, et)
-	}
-
-	transfers := make([]valueobject.Transfer, len(wua.Transfers))
-	for _, tra := range wua.Transfers {
-		tr, err := tra.ToValueObject()
-		if err != nil {
-			return nil, err
-		}
-		transfers = append(transfers, *tr)
-	}
-
-	wallet.SetID(person.UUID)
-	wallet.SetAccount(account)
-	wallet.SetEntries(entries...)
-	wallet.SetTransfers(transfers...)
-
-	return &wallet, nil
 }
