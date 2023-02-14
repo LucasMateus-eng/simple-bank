f2bd704ffa97a7ca16e669667ca9898c72af39ab
Lucas Neris <lucas.lima.neris@gmail.com>
2023-02-14 14:03:14 -0300

    refactor(simple-bank): mudança do parâmetro para as aperações de criação e atualização de Wallet

--- application/ports/secondary/wallet/repository.go
+++ application/ports/secondary/wallet/repository.go
@@ -2,14 +2,15 @@ package wallet
 
 import (
 	"github.com/LucasMateus-eng/simple-bank/application/aggregate"
+	"github.com/LucasMateus-eng/simple-bank/application/entity"
 	valueobject "github.com/LucasMateus-eng/simple-bank/application/value_object"
 	"github.com/google/uuid"
 )
 
 type WalletRepository interface {
 	Get(uuid.UUID) (aggregate.Wallet, error)
-	Add(aggregate.Wallet) error
-	Update(aggregate.Wallet) error
+	Add(entity.Person) error
+	Update(entity.Person) error
 	Delete(uuid.UUID) error
 	Transfer(valueobject.Transfer) error
 	Deposit(valueobject.Transfer) error
